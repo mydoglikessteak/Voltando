6daf9576acf4185d72fa90c92ecd2d7bc72fe2c8
vini <viniciusalex0303@gmail.com>
2025-01-03 10:46:46 -0300

    conhecimentos

--- hellogit.py
+++ hellogit.py
@@ -1,3 +1,3 @@
-print("hello git voltando aos trilhos em 03/01/2025 ")
 variavelMaconha = "gellatokush"
+print(f"hello git voltando aos trilhos em 03/01/2025, {variavelMaconha} e bom dms ")
 print(variavelMaconha)
